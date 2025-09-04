f464c23b8956c52855b0f38dfeb0e118debbcbb8
JunSeong <JunSeong-github>
2025-09-05 01:43:31 +0900

    깃액션 테스트코드

--- mainProject/src/test/java/com/example/mainproject/MainProjectApplicationTests.java
+++ mainProject/src/test/java/com/example/mainproject/MainProjectApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.mainproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.Test;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MainProjectApplicationTests {
-
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
+
+
+//package com.example.mainproject;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MainProjectApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
